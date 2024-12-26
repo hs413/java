a9a07ae9bee45492c4f29718b61bf6b0a0037ed2
rtg <rtg2541@gmail.com>
2024-12-26 21:41:30 +0900

    이팩티브 자바: 제네릭 타입 예시(적용 전)

---
+++ effective-java/05-Generics/test/src/stack/Stack.java
@@ -0,0 +1,38 @@
+package stack;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+
+        Object o = elements[--size];
+        elements[size] = null;
+        return o;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, size * 2 + 1);
+        }
+    }
+}
