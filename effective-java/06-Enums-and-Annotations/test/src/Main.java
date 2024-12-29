50776c26e5bdf4e1f83672f5d2cec3465a1c4cd5
rtg <rtg2541@gmail.com>
2024-12-29 15:12:53 +0900

    이팩티브 자바: enumMap 예시

--- effective-java/06-Enums-and-Annotations/test/src/Main.java
+++ effective-java/06-Enums-and-Annotations/test/src/Main.java
@@ -1,3 +1,11 @@
+//import Plant.LifeCycle;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
 
 public class Main {
@@ -5,7 +13,8 @@ public class Main {
     public static void main(String[] args) {
 //        planetTest(11);
 //        operationTest();
-        payrollTest();
+//        payrollTest();
+        plantTest();
     }
 
     public static void planetTest(double mass) {
@@ -34,5 +43,28 @@ public class Main {
         System.out.println(PayrollDay.Monday.pay(480, 2));
         System.out.println(PayrollDay.Monday.pay(960, 2));
         System.out.println(PayrollDay.Sunday.pay(480, 2));
+    }
+
+    public static void plantTest() {
+        Map<Plant.LifeCycle, Set<Plant>> plantsByLifeCycle =
+                new EnumMap<>(Plant.LifeCycle.class);
+
+        for (Plant.LifeCycle lifeCycle : Plant.LifeCycle.values()) {
+            plantsByLifeCycle.put(lifeCycle, new HashSet<>());
+        }
+
+       /* for (Plant p : garden) {
+            plantsByLifeCycle.get(p.lifeCycle).add(p);
+        }
+
+        Arrays.stream(garden)
+                .collect(Collectors.groupingBy(p -> p.lifeCycle));
+
+        Arrays.stream(garden)
+                .collect(Collectors.groupingBy(p -> p.lifeCycle,
+                        () -> new EnumMap<>(LifeCycle.class), Collectors.toSet()));*/
+
+        System.out.println(plantsByLifeCycle);
+
     }
 }
