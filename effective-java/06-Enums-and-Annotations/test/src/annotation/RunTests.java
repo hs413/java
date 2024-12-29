525c09bbc6c370fb523f10a8b4f2f43a6fbdaf1c
rtg <rtg2541@gmail.com>
2024-12-29 16:37:50 +0900

    이팩티브 자바: 애너테이션 예시2

--- effective-java/06-Enums-and-Annotations/test/src/annotation/RunTests.java
+++ effective-java/06-Enums-and-Annotations/test/src/annotation/RunTests.java
@@ -7,7 +7,8 @@ public class RunTests {
     public static void main(String[] args) throws Exception {
         int tests = 0;
         int passed = 0;
-        Class<?> testClass = Class.forName("annotation.Sample");
+//        Class<?> testClass = Class.forName("annotation.Sample");
+        Class<?> testClass = Class.forName("annotation.Sample2");
 
         for (Method m : testClass.getDeclaredMethods()) {
             if (m.isAnnotationPresent(Test.class)) {
@@ -18,7 +19,14 @@ public class RunTests {
                     passed++;
                 } catch (InvocationTargetException e) {
                     Throwable t = e.getCause();
-                    System.out.println(m + " 실패: " + t);
+                    Class<? extends Throwable> excType = m.getAnnotation(ExceptionTest.class).value();
+
+                    if (excType.isInstance(t)) {
+                        passed++;
+                    } else {
+                        System.out.printf("테스트 %s 실패: 기대한 에외 %s, 발생한 예외 %s%n",
+                                m, excType.getName(), t);
+                    }
                 } catch (Exception e) {
                     System.out.println("잘못 사용한 @Test: " + m);
                 }
