d62bc17a0977a43b9a59ff80a0561b61e617889a
rtg <rtg2541@gmail.com>
2025-02-05 21:12:52 +0900

    모던자바 컬렉션 API: replaceAll

--- modern-java/08-collection-api/example/src/BasicExample.java
+++ modern-java/08-collection-api/example/src/BasicExample.java
@@ -1,10 +1,18 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import transaction.Trader;
 import transaction.Transaction;
 
 public class BasicExample {
-    public static void test() {
+    public static void removeIfTest() {
         Trader raoul = new Trader("Raoul", "Cambridge");
         Trader mario = new Trader("Mario", "Milan");
         Trader alan = new Trader("Alan", "Cambridge");
@@ -38,4 +46,32 @@ public class BasicExample {
         transactions.removeIf(transaction -> Character.isDigit(transaction.getReferenceCode().charAt(0)));
     }
 
+    public static void replaceAllTest() {
+        List<String> referenceCodes = List.of("a12", "C14", "b13");
+
+        // 새로운 컬렉션을 생성
+        referenceCodes.stream()
+                .map(code -> Character.toUpperCase(code.charAt(0)) + code.substring(1))
+                .collect(Collectors.toUnmodifiableList())
+                .forEach(System.out::println);
+
+        // 기존 컬렉션 변경
+        for (ListIterator<String> iterator = referenceCodes.listIterator(); iterator.hasNext();) {
+            String code = iterator.next();
+            iterator.set(Character.toUpperCase(code.charAt(0)) + code.substring(1));
+        }
+
+        // replaceAll
+        referenceCodes.replaceAll(code -> Character.toUpperCase(code.charAt(0)) + code.substring(1));
+    }
+;
+
+
+
+
+
+
+
+
+    }
 }
