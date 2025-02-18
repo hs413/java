84d95898e65ed62a025ca1fae069806fb0186683
rtg <rtg2541@gmail.com>
2025-02-18 21:29:00 +0900

    모던자바 리팩터링: 익명 클래스를 람다로

---
+++ modern-java/09-refactoring/example/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Runnable r1 = new Runnable() {
+            public void run() {
+                System.out.println("hello");
+            }
+        };
+
+        Runnable r2 = () -> System.out.println("hello");
+
+        int a = 10;
+        Runnable r11 = () -> {
+            // 람다는 블록 스코프가 적용되지 않는다?
+            // 변수를 가릴 수 없다?
+            // int a = 2; // 컴파일 에러
+            System.out.println(a);
+        };
+
+        Runnable r22 = new Runnable() {
+            @Override
+            public void run() {
+                int a = 2;
+                System.out.println(a);
+            }
+        };
+
+    }
+
+
+}
+
+
+
