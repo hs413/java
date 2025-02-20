d010f8d63617dec8d599975b81edcf6521040c4e
rtg <rtg2541@gmail.com>
2025-02-20 22:29:00 +0900

    모던자바 리팩터링: 전략 패턴(스트림)

--- modern-java/09-refactoring/example/src/design/strategy/Validator.java
+++ modern-java/09-refactoring/example/src/design/strategy/Validator.java
@@ -12,11 +12,20 @@ public class Validator {
     }
 
     public static void test() {
-        Validator numericValidator = new Validator(new IsNumeric());
+        /*Validator numericValidator = new Validator(new IsNumeric());
         boolean b1 = numericValidator.execute("aaa");
         System.out.println(b1);
 
         Validator allLowerCaseValidator = new Validator(new IsAllLowerCase());
+        boolean b2 = allLowerCaseValidator.execute("aaa");
+        System.out.println(b2);*/
+
+        // 스트림 사용: 전략에 사용되는 클래스를 새로 구현하지 않아도 된다.
+        Validator numericValidator = new Validator((String s) -> s.matches("[0-9]+"));
+        boolean b1 = numericValidator.execute("aaa");
+        System.out.println(b1);
+
+        Validator allLowerCaseValidator = new Validator((String s) -> s.matches("[a-z]+"));
         boolean b2 = allLowerCaseValidator.execute("aaa");
         System.out.println(b2);
     }
