4dfad5954fdff24599862d23741ccab85b381e27
rtg <rtg2541@gmail.com>
2025-02-27 21:54:45 +0900

    모던자바 리팩터링: 람다 테스팅 예시2

--- modern-java/09-refactoring/example/src/testing/Point.java
+++ modern-java/09-refactoring/example/src/testing/Point.java
@@ -1,6 +1,9 @@
 package testing;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Point {
     private final int x;
@@ -21,6 +24,7 @@ public class Point {
         return new Point(this.x + x, this.y);
     }
 
+    @Test
     public void testMoveRightBy() throws Exception {
         Point p1 = new Point(5, 5);
         Point p2 = p1.moveRightBy(10);
@@ -31,11 +35,27 @@ public class Point {
     public final static Comparator<Point> compareByXAndThenY =
             Comparator.comparing(Point::getX).thenComparing(Point::getY);
 
+    @Test
     public void testComparingTwoPoints() throws Exception {
         Point p1 = new Point(10, 15);
         Point p2 = new Point(10, 20);
 
         int result = Point.compareByXAndThenY.compare(p1, p2);
         assertTrue(result < 0);
+    }
+
+    public static List<Point> moveAllPointsRightBy(List<Point> points, int x) {
+        return points.stream()
+                .map(p -> new Point(p.getX() + x, p.getY()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Test
+    public void testMoveAllPointsRightBy() throws Exception {
+        List<Point> points = Arrays.asList(new Point(5, 5), new Point(10, 5));
+        List<Point> expectedPoints = Arrays.asList(new Point(15, 5), new Point(20, 5));
+        List<Point> newPoints = Point.moveAllPointsRightBy(points, 10);
+
+        assertEquals(expectedPoints, newPoints);
     }
 }
