04f0e60a89ac0cd704abfad8d94a821836a6751d
rtg <rtg2541@gmail.com>
2025-11-28 18:46:12 +0900

    factory pattern 1

---
+++ design-pattern/04-factory-pattern/src/Pizza.java
@@ -0,0 +1,36 @@
+public class Pizza {
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza = new Pizza();
+
+
+        if(type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new GreekPizza();
+        }
+
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+
+
+
+
+    public void prepare() {
+
+    }
+
+    public void bake() {
+
+    }
+
+    public void cut() {
+    }
+
+    public void box() {}
+}
