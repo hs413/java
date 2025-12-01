09e69a7feef05d47860673ee0b7aaedf1957b12f
rtg <rtg2541@gmail.com>
2025-12-01 20:44:05 +0900

    singleton pattern 2

--- design-pattern/05-singleton-pattern/src/Singleton.java
+++ design-pattern/05-singleton-pattern/src/Singleton.java
@@ -1,12 +1,20 @@
 public class Singleton {
-    private static Singleton instance;
+//    private static Singleton instance;
+//
+//    private Singleton() {}
+//
+//    public static Singleton getInstance() {
+//        if (instance == null) {
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+
+    private static Singleton instance = new Singleton();
 
     private Singleton() {}
 
     public static Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton();
-        }
         return instance;
     }
 
