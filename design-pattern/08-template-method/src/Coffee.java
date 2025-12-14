5ecfd920b61b187ce5a78139d0260b59d125d96b
rtg <rtg2541@gmail.com>
2025-12-14 19:47:41 +0900

    template method pattern 1

---
+++ design-pattern/08-template-method/src/Coffee.java
@@ -0,0 +1,27 @@
+public class Coffee {
+    void prepareRecipe() {
+        boilWater();
+        brewCoffeeGrinds();
+        pourInCup();
+        addSugarAndMilk();
+    }
+
+    public void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    public void brewCoffeeGrinds() {
+        System.out.println("Brewing coffee grinds");
+    }
+
+    public void pourInCup() {
+        System.out.println("Pouring cup");
+    }
+
+    public void addSugarAndMilk() {
+        System.out.println("Adding sugar and milk");
+    }
+
+
+}
+
---
+++ design-pattern/08-template-method/src/Tea.java
@@ -0,0 +1,26 @@
+public class Tea {
+    void prepareRecipe() {
+        boilWater();
+        steepTeaBag();
+        pourInCup();
+        addLemon();
+    }
+
+    public void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    public void steepTeaBag() {
+        System.out.println("Steeping tea bag");
+    }
+
+    public void addLemon() {
+        System.out.println("AddLemon");
+    }
+
+    public void pourInCup() {
+        System.out.println("Pouring cup");
+    }
+
+}
+
