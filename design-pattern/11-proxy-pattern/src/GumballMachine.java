325d745a6cb9d1d59408259c6c0a77af676c2c9d
rtg <rtg2541@gmail.com>
2026-01-05 07:58:13 +0900

    proxy pattern 1

---
+++ design-pattern/11-proxy-pattern/src/GumballMachine.java
@@ -0,0 +1,11 @@
+public class GumballMachine {
+    String location;
+
+    public GumballMachine(String location, int count) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
---
+++ design-pattern/11-proxy-pattern/src/GumballMonitor.java
@@ -0,0 +1,11 @@
+public class GumballMonitor {
+    GumballMachine gumballMachine;
+
+    public GumballMonitor(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void report() {
+        System.out.println("기계 위치: " + gumballMachine.getLocation());
+    }
+}
